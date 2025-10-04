07e639c52963400815485a392aefc91f8f9304ab
Raheel Asghar <raheel.ghauri@nayatel.com>
2025-10-04 13:39:23 +0500

    setup

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('', views.dashboard1, name='dashboard1'),
+    path('dashboard2/', views.dashboard2, name='dashboard2'),
+    path('dashboard3/', views.dashboard3, name='dashboard3'),
+]
---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def dashboard1(request):
+    context = {'dashboard_number': 1}
+    return render(request, 'dashboard.html',context)
+
+def dashboard2(request):
+    context = {'dashboard_number': 2}
+    return render(request, 'dashboard.html',context)
+
+def dashboard3(request):
+    context = {'dashboard_number': 2}
+    return render(request, 'dashboard.html',context)
